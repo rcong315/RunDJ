3efa61ca426eecf88d7979ca44767f7bbe454749
rcong315 <richardcong635@gmail.com>
2025-05-21 22:16:37 -0700

    Add sentry

--- RunDJ/RunDJApp.swift
+++ RunDJ/RunDJApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import Sentry
+
 import SwiftData
 import os.log
 
@@ -42,6 +44,31 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        SentrySDK.start { options in
+            options.dsn = "https://17790a7e5b233847b12b14cb8c3bcad5@o4509359544008704.ingest.us.sentry.io/4509359546105856"
+            options.debug = true // Enabled debug when first installing is always helpful
+
+            // Adds IP for users.
+            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+            options.sendDefaultPii = true
+
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+
+            // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+            options.configureProfiling = {
+                $0.sessionSampleRate = 1.0 // We recommend adjusting this value in production.
+                $0.lifecycle = .trace
+            }
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+        }
+        // Remove the next line after confirming that your Sentry integration is working.
+        SentrySDK.capture(message: "This app uses Sentry! :)")
+
         logger.info("Application finished launching")
         return true
     }
